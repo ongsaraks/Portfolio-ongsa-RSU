e567e1090a66b6e70f0d4d7751390b0d382489e2
ONGSA RAKSALAM <ongsaraks@gmail.com>
2022-06-12 14:36:52 +0700

    Add pic
    Change pig in home.jsx

--- ongsa/src/components/Home.jsx
+++ ongsa/src/components/Home.jsx
@@ -16,7 +16,7 @@ return (
 whileInView={{ x: 0 }}
 transition={{ type: "spring", stiffness: 100 }}
 >
-<img src="/img/GDafteRz.jpg" alt="" />
+<img src="https://scontent.fbkk29-1.fna.fbcdn.net/v/t1.6435-9/137303855_861636761268716_2707164544925441675_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=174925&_nc_eui2=AeGvAajNJL9R0wqhvspMNgZuT5-2o26u8nNPn7ajbq7ycyOczhPOUVYcTEtqS7toLJGfUN75UeQ97FvRtSx-n_9q&_nc_ohc=IHqHtHgz-rAAX9edcXN&_nc_ht=scontent.fbkk29-1.fna&oh=00_AT99x7M4FqGls7AADkFk1N472arf2MI26zhhRuQAnD1Dzg&oe=62CA5B70" alt="" />
 </motion.div>
 </div>
 </div>
